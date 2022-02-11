0ef308efcdc4d5bea3d6d80d3bf44a2aeca434fd
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2022-02-11 19:46:56 -0300

    Classe, dados mockados e tela criada

---
+++ lib/models/product.dart
@@ -0,0 +1,25 @@
+//classe
+class Product {
+  //variáveis
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  //construtor
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  //método para alternar o favorito
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+  }
+}
