64ed14fdac7bc2957614730726f0d6d1ebb42abb
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2022-02-11 20:58:42 -0300

    Começando a fazer a tela principal

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  const ProductsOverviewScreen({ Key? key }) : super(key: key);
+  final List<Product> loadedProduct =
+      dummyProducts; //importando os dados mockados
+
+  ProductsOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Loja'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProduct.length, //quantidade de produto no grid
+        //o que vai ser mostrado
+        itemBuilder: ((context, index) => ProductItem(
+              product: loadedProduct[index],
+            )),
+        //como vai ser mostrado
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
     );
   }
 }
