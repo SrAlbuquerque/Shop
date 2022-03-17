bb908d1390931b122f9e9fb0b7e1d39838d27806
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2022-03-16 22:08:36 -0300

    Começando a fazer a tela de detalhes do produto

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -11,8 +11,37 @@ class ProductDetailScreen extends StatelessWidget {
         as Product; //argumento que foi passado pela rota
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          product.name,
+        title: Text(product.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'R\$ ${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
         ),
       ),
     );
