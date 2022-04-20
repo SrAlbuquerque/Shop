1f0d7e2ed3a82bb559f50463f913a98c7320dfb3
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2022-04-20 15:23:26 -0300

    Tratando error de salvar produto

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -82,7 +82,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
     Provider.of<ProductList>(
       context,
       listen: false,
-    ).saveProduct(_formData).then((value) {
+    ).saveProduct(_formData).catchError((error) {
+      return showDialog<void>(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Ocorreu um erro!'),
+          content: Text(error.toString()),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Ok'),
+            ),
+          ],
+        ),
+      );
+    }).then((value) {
       setState(() {
         _isLoading = false;
       });
