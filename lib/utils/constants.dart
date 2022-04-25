18805ad7a62c359652260aa9f75bcdb2a6235cc6
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2022-04-25 16:16:25 -0300

    Fazendo com que os pedidos vá para o firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const PRODUCT_BASE_URL =
       'https://shop-curso-flutter-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL =
+      'https://shop-curso-flutter-default-rtdb.firebaseio.com/orders';
 }
