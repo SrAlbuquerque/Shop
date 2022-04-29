78496a9da96f03645ed004c1978d295af276cd8f
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2022-04-29 08:56:06 -0300

    Permanecer logado enquanto o token ainda estiver válido implementado

--- lib/pages/auth_or_home_page.dart
+++ lib/pages/auth_or_home_page.dart
@@ -10,6 +10,19 @@ class AuthOrHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+    return FutureBuilder(
+      future: auth.tryAutoLogin(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.error != null) {
+          return const Center(
+            child: Text('Ocorreu um erro!'),
+          );
+        } else {
+          return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+        }
+      },
+    );
   }
 }
